e0e7f62d2f5c3af35322fd89969142f4af00b7f3
unknown <casartellin@1812-casartellin.allos.local>
2020-01-10 10:14:35 +0100

    Aggiunto gitignore e comandi vocali

--- bot.js
+++ bot.js
@@ -1,15 +1,120 @@
 const Discord = require('discord.js');
-
+const {
+	prefix,
+	token,
+} = require('./config.json');
+const ytdl = require('ytdl-core');
 const client = new Discord.Client();
+const queue = new Map();
 
-client.on('ready', () => {
+client.once('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
+client.once('reconnecting', () => {
+ console.log('Reconnecting!');
 });
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.once('disconnect', () => {
+ console.log('Disconnect!');
+});
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	const serverQueue = queue.get(message.guild.id);
+
+	if (message.content.startsWith(`${prefix}play`)) {
+		execute(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}skip`)) {
+		skip(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}stop`)) {
+		stop(message, serverQueue);
+		return;
+	} else {
+		message.channel.send('You need to enter a valid command!')
+	}
+})
+
+async function execute(message, serverQueue) {
+	const args = message.content.split(' ');
+	const voiceChannel = message.member.voiceChannel;
+	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+		return message.channel.send('I need the permissions to join and   speak in your voice channel!');
+	}
+	const songInfo = await ytdl.getInfo(args[1]);
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+	};
+	
+	if (!serverQueue) {
+		// Creating the contract for our queue
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+		// Setting the queue using our contract
+		queue.set(message.guild.id, queueContruct);
+		// Pushing the song to our songs array
+		queueContruct.songs.push(song);
+
+		try {
+			// Here we try to join the voicechat and save our connection into our object.
+			var connection = await voiceChannel.join();
+			queueContruct.connection = connection;
+			// Calling the play function to start a song
+			play(message.guild, queueContruct.songs[0]);
+		} catch (err) {
+			// Printing the error message if the bot fails to join the voicechat
+			console.log(err);
+			queue.delete(message.guild.id);
+			return message.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`${song.title} has been added to the queue!`);
+	}
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', () => {
+			console.log('Music ended!');
+			// Deletes the finished song from the queue
+			serverQueue.songs.shift();
+			// Calls the play function again with the next song
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+
+function skip(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+	serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
+
+client.login(process.env.BOT_TOKEN);
